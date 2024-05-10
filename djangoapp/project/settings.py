f311df6f08d6dfb6e9c2cc0ed97b6056f797ea02
juarezrbr1 <juarezribeiro94@gmail.com>
2024-05-10 20:25:31 -0300

    exclusao do app

--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -24,8 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'appblog'
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
